af5ed932fa5a9983869e0e4cb661409f83e72fb4
sendoa.moronta <sendoa.moronta.next@bbva.com>
2019-01-14 16:14:16 +0100

    ARCHER-159 use apache serialization in serde

--- kst-archer-lib-common/src/main/java/com/bbva/common/utils/ByteArrayValue.java
+++ kst-archer-lib-common/src/main/java/com/bbva/common/utils/ByteArrayValue.java
@@ -1,11 +1,9 @@
 package com.bbva.common.utils;
 
+import org.apache.commons.lang.SerializationUtils;
 import org.apache.kafka.common.utils.Bytes;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.nio.ByteBuffer;
 
 public class ByteArrayValue {
@@ -55,57 +53,14 @@ public class ByteArrayValue {
     public static class Serde {
 
         public static <T> T deserializeAs(final Class<T> classType, final byte[] data) {
-            final T deserializeData;
-            if (classType == ByteBuffer.class) {
-                deserializeData = (T) ByteBuffer.wrap(data);
-            } else if (classType == Bytes.class) {
-                deserializeData = (T) Bytes.wrap(data);
-            } else if (classType == String.class) {
-                deserializeData = (T) new String(data);
-            } else if (classType == Boolean.class) {
-                deserializeData = (T) Boolean.valueOf(data.length > 0 && data[0] != 0);
-            } else if (classType == Integer.class) {
-                deserializeData = (T) (Integer) ByteBuffer.wrap(data).getInt();
-            } else if (classType == Long.class) {
-                deserializeData = (T) (Long) ByteBuffer.wrap(data).getLong();
-            } else if (classType == Float.class) {
-                deserializeData = (T) (Float) ByteBuffer.wrap(data).getFloat();
-            } else {
-                throw new ClassCastException("Inconvertible types");
-            }
-            return deserializeData;
+            return (T) SerializationUtils.deserialize(data);
         }
 
         public static <T> byte[] serialize(final T data) {
-            final byte[] serializeData;
             if (data instanceof byte[]) {
-                serializeData = (byte[]) data;
-            } else if (data instanceof ByteBuffer) {
-                serializeData = ((ByteBuffer) data).array();
-            } else if (data instanceof Bytes) {
-                serializeData = ((Bytes) data).get();
-            } else if (data instanceof String) {
-                serializeData = ((String) data).getBytes();
-            } else if (data instanceof Boolean) {
-                serializeData = new byte[]{(byte) ((Boolean) data ? 1 : 0)};
-            } else if (data instanceof Integer) {
-                serializeData = ByteBuffer.allocate(4).putInt((Integer) data).array();
-            } else if (data instanceof Long) {
-                serializeData = ByteBuffer.allocate(8).putLong((Long) data).array();
-            } else if (data instanceof Float) {
-                serializeData = ByteBuffer.allocate(4).putFloat((Float) data).array();
-            } else {
-                try (final ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
-                    final ObjectOutput out;
-                    out = new ObjectOutputStream(bos);
-                    out.writeObject(data);
-                    out.flush();
-                    serializeData = bos.toByteArray();
-                } catch (final IOException e) {
-                    throw new ClassCastException("Inconvertible types: data is " + data.getClass().getName());
-                }
+                return (byte[]) data;
             }
-            return serializeData;
+            return SerializationUtils.serialize((Serializable) data);
         }
 
     }
