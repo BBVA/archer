4ffdac19ddfe03c5a42a94901e12c37a4488635a
sendoa.moronta <sendoa.moronta.next@bbva.com>
2019-02-25 12:02:58 +0100

    ARCHER-169 add name to event

--- kst-archer-lib-ddd/src/main/java/com/bbva/ddd/domain/events/write/Event.java
+++ kst-archer-lib-ddd/src/main/java/com/bbva/ddd/domain/events/write/Event.java
@@ -33,26 +33,30 @@ public class Event {
     }
 
     public <V extends SpecificRecord> EventRecordMetadata send(final String productorName, final V data, final ProducerCallback callback) {
-        return generateEvent(null, productorName, data, callback, HelperDomain.get().isReplayMode(), null);
+        return generateEvent(null, productorName, data, callback, HelperDomain.get().isReplayMode(), null, null);
+    }
+
+    public <V extends SpecificRecord> EventRecordMetadata send(final String productorName, final V data, final ProducerCallback callback, final String name) {
+        return generateEvent(null, productorName, data, callback, HelperDomain.get().isReplayMode(), null, name);
     }
 
     public <V extends SpecificRecord> EventRecordMetadata send(final String key, final String productorName, final V data,
                                                                final ProducerCallback callback) {
-        return generateEvent(key, productorName, data, callback, HelperDomain.get().isReplayMode(), null);
+        return generateEvent(key, productorName, data, callback, HelperDomain.get().isReplayMode(), null, null);
     }
 
     public <V extends SpecificRecord> EventRecordMetadata send(
             final String productorName, final V data, final ProducerCallback callback, final boolean replay, final String referenceId) {
-        return generateEvent(null, productorName, data, callback, replay, referenceId);
+        return generateEvent(null, productorName, data, callback, replay, referenceId, null);
     }
 
     private <V extends SpecificRecord> EventRecordMetadata generateEvent(
             String key, final String productorName, final V record,
-            final ProducerCallback callback, final boolean replay, final String referenceId) {
+            final ProducerCallback callback, final boolean replay, final String referenceId, final String name) {
         logger.debug("Generating event by {}", productorName);
         key = (key != null) ? key : UUID.randomUUID().toString();
 
-        final RecordHeaders headers = headers(productorName, replay, referenceId);
+        final RecordHeaders headers = headers(productorName, replay, referenceId, name);
 
         final Future<RecordMetadata> result = producer.add(new PRecord<>(topic, key, record, headers), callback);
 
@@ -69,7 +73,7 @@ public class Event {
         return recordedMessageMetadata;
     }
 
-    private static RecordHeaders headers(final String productorName, final boolean replay, final String referenceId) {
+    private static RecordHeaders headers(final String productorName, final boolean replay, final String referenceId, final String name) {
 
         final RecordHeaders recordHeaders = new RecordHeaders();
         recordHeaders.add(CRecord.TYPE_KEY, new ByteArrayValue(Event.TYPE_EVENT_VALUE));
@@ -77,6 +81,10 @@ public class Event {
         recordHeaders.add(CRecord.FLAG_REPLAY_KEY, new ByteArrayValue(replay));
         if (referenceId != null) {
             recordHeaders.add(EventRecord.REFERENCE_ID, new ByteArrayValue(referenceId));
+        }
+
+        if (name != null) {
+            recordHeaders.add(EventRecord.NAME_KEY, new ByteArrayValue(name));
         }
 
         logger.debug("CRecord getList: {}", recordHeaders.toString());
