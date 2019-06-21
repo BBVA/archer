5ed3415455987945c3c0ef537f6b149370d66cb5
sendoa.moronta <sendoa.moronta.next@bbva.com>
2019-06-21 09:38:04 +0200

    ARCHER-238 disable logs appender in tests

--- kst-archer-lib-log/src/main/java/com/bbva/logging/LoggerFactory.java
+++ kst-archer-lib-log/src/main/java/com/bbva/logging/LoggerFactory.java
@@ -1,42 +1,60 @@
 package com.bbva.logging;
 
-import com.bbva.common.config.AppConfiguration;
-import com.bbva.common.config.ApplicationConfig;
-import com.bbva.common.producers.CachedProducer;
 import org.apache.log4j.MDC;
 import org.apache.log4j.PropertyConfigurator;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class LoggerFactory {
     static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(LoggerFactory.class);
 
     private static final String LOG4J_PROPS_PATH = "custom_log4j.properties";
-    private final ApplicationConfig applicationConfig;
-    CachedProducer logsProducer;
+    private static final String LOG4J_PROPS_PATH_TEST = "custom_log4j_test.properties";
 
     private static LoggerFactory instance;
 
     private LoggerFactory() {
-        applicationConfig = new AppConfiguration().init();
-        logsProducer = new CachedProducer(applicationConfig);
         try {
-            final InputStream inputStream =
-                    getClass().getClassLoader().getResourceAsStream(LOG4J_PROPS_PATH);
-            final Properties logPorperties = new Properties();
-            if (inputStream != null) {
-                logPorperties.load(inputStream);
-            }
-            PropertyConfigurator.configure(logPorperties);
+
+            PropertyConfigurator.configure(getConfigByRuntime());
 
             MDC.put("hostName", InetAddress.getLocalHost().getHostName());
 
         } catch (final IOException e) {
             logger.error("Failed to load log4j properties from file", e);
         }
+    }
+
+    private Properties getConfigByRuntime() throws IOException {
+
+        final StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        final List<StackTraceElement> list = Arrays.asList(stackTrace);
+
+        final String configFile = getConfigFile(list);
+
+        final InputStream inputStream =
+                getClass().getClassLoader().getResourceAsStream(configFile);
+
+        final Properties logPorperties = new Properties();
+        if (inputStream != null) {
+            logPorperties.load(inputStream);
+        }
+
+        return logPorperties;
+    }
+
+    private String getConfigFile(final List<StackTraceElement> list) {
+        for (final StackTraceElement element : list) {
+            if (element.getClassName().startsWith("org.junit.")) {
+                return LOG4J_PROPS_PATH_TEST;
+            }
+        }
+        return LOG4J_PROPS_PATH;
     }
 
     public static Logger getLogger(final Class mainClass) {
