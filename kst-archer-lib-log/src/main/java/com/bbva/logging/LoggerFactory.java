8362566f6ddbec4a0cdc7175fcfec80c92884810
sendoa.moronta <sendoa.moronta.next@bbva.com>
2019-06-21 12:01:22 +0200

    ARCHER-238 configure log with env vars

--- kst-archer-lib-log/src/main/java/com/bbva/logging/LoggerFactory.java
+++ kst-archer-lib-log/src/main/java/com/bbva/logging/LoggerFactory.java
@@ -6,22 +6,27 @@ import org.apache.log4j.PropertyConfigurator;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetAddress;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Properties;
 
 public class LoggerFactory {
     static final org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(LoggerFactory.class);
 
     private static final String LOG4J_PROPS_PATH = "custom_log4j.properties";
-    private static final String LOG4J_PROPS_PATH_TEST = "custom_log4j_test.properties";
+    private static final String CUSTOM_LOG_PROPS_PATH = "appender_log4j.properties";
 
     private static LoggerFactory instance;
 
     private LoggerFactory() {
         try {
+            final Properties logProperties = getConfigByRuntime(LOG4J_PROPS_PATH);
 
-            PropertyConfigurator.configure(getConfigByRuntime());
+            if (System.getenv("LOG_APPENDER_CONFIG") != null && System.getenv("LOG_APPENDER_CONFIG").indexOf("=") > 0) {
+                final String[] appenderConfig = System.getenv("LOG_APPENDER_CONFIG").split("=");
+                logProperties.put(String.format("log4j.logger.%s", appenderConfig[0]), appenderConfig[1]);
+                logProperties.putAll(getConfigByRuntime(CUSTOM_LOG_PROPS_PATH));
+            }
+
+            PropertyConfigurator.configure(logProperties);
 
             MDC.put("hostName", InetAddress.getLocalHost().getHostName());
 
@@ -30,31 +35,18 @@ public class LoggerFactory {
         }
     }
 
-    private Properties getConfigByRuntime() throws IOException {
-
-        final StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        final List<StackTraceElement> list = Arrays.asList(stackTrace);
-
-        final String configFile = getConfigFile(list);
+    private Properties getConfigByRuntime(final String filePath) throws IOException {
 
         final InputStream inputStream =
-                getClass().getClassLoader().getResourceAsStream(configFile);
+                getClass().getClassLoader().getResourceAsStream(filePath);
 
         final Properties logPorperties = new Properties();
+
         if (inputStream != null) {
             logPorperties.load(inputStream);
         }
 
         return logPorperties;
-    }
-
-    private String getConfigFile(final List<StackTraceElement> list) {
-        for (final StackTraceElement element : list) {
-            if (element.getClassName().startsWith("org.junit.")) {
-                return LOG4J_PROPS_PATH_TEST;
-            }
-        }
-        return LOG4J_PROPS_PATH;
     }
 
     public static Logger getLogger(final Class mainClass) {
