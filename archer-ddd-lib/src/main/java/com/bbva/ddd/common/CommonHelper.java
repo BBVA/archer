02a8aafb94c200ffd846df5dce482df3656015d7
Juan Ferrer <juan.ferrer2@bbva.com>
2019-07-19 10:07:21 +0200

    [ARCHER-240]

--- archer-ddd-lib/src/main/java/com/bbva/ddd/common/CommonHelper.java
+++ archer-ddd-lib/src/main/java/com/bbva/ddd/common/CommonHelper.java
@@ -5,12 +5,8 @@ import com.bbva.common.utils.TopicManager;
 import com.bbva.ddd.domain.commands.write.Command;
 import com.bbva.ddd.domain.events.write.Event;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CommonHelper {
 
@@ -26,53 +22,74 @@ public class CommonHelper {
         instance = this;
     }
 
-
+    /**
+     * It gets the current configuration
+     *
+     * @return current configuration
+     */
     public ApplicationConfig getConfig() {
         return applicationConfig;
     }
 
-    public synchronized Command persistsCommandTo(final String baseName) {
-        return this.writeCommandTo(baseName, true);
+    /**
+     * It persists a command in the event store. Persistent commands are usually used to store data that came from out of
+     * system.
+     *
+     * @param name Command name of the stream without suffix "_command"
+     * @return An instance of Command producer
+     */
+    public synchronized Command persistsCommandTo(final String name) {
+        return this.writeCommandTo(name, true);
     }
 
-    public synchronized Command sendCommandTo(final String baseName) {
-        return this.writeCommandTo(baseName, false);
+    /**
+     * It sends a command to the event store. Non-persistent commands are usually used to send data inside of the system
+     *
+     * @param name Command name of the stream without suffix "_command"
+     * @return An instance of Command producer
+     */
+    public synchronized Command sendCommandTo(final String name) {
+        return this.writeCommandTo(name, false);
     }
 
-    public synchronized Command writeCommandTo(final String baseName, final boolean persist) {
-        if (cacheCommandPersistance.containsKey(baseName)) {
-            return cacheCommandPersistance.get(baseName);
+    /**
+     * It sends a command to the event store.
+     *
+     * @param name    Command name without suffix "_command"
+     * @param persist if the command persists
+     * @return An instance of Command producer
+     */
+    public synchronized Command writeCommandTo(final String name, final boolean persist) {
+        if (cacheCommandPersistance.containsKey(name)) {
+            return cacheCommandPersistance.get(name);
         } else {
             final Map<String, String> commandTopic = new HashMap<>();
-            commandTopic.put(baseName + ApplicationConfig.COMMANDS_RECORD_NAME_SUFFIX, ApplicationConfig.COMMANDS_RECORD_TYPE);
+            commandTopic.put(name + ApplicationConfig.COMMANDS_RECORD_NAME_SUFFIX, ApplicationConfig.COMMANDS_RECORD_TYPE);
             TopicManager.createTopics(commandTopic, applicationConfig);
 
-            final Command command = new Command(baseName, applicationConfig, persist);
-            cacheCommandPersistance.put(baseName, command);
+            final Command command = new Command(name, applicationConfig, persist);
+            cacheCommandPersistance.put(name, command);
             return command;
         }
     }
 
-    public synchronized Event sendEventTo(final String baseName) {
-        if (cacheEvents.containsKey(baseName)) {
-            return cacheEvents.get(baseName);
+    /**
+     * It sends an event to the event store.
+     *
+     * @param name Event name of the stream without suffix "_event"
+     * @return An instance of Event producer
+     */
+    public synchronized Event sendEventTo(final String name) {
+        if (cacheEvents.containsKey(name)) {
+            return cacheEvents.get(name);
         } else {
-            final Map<String, String> commandTopic = new HashMap<>();
-            commandTopic.put(baseName + ApplicationConfig.EVENTS_RECORD_NAME_SUFFIX, ApplicationConfig.EVENTS_RECORD_TYPE);
-            TopicManager.createTopics(commandTopic, applicationConfig);
+            final Map<String, String> eventTopic = new HashMap<>();
+            eventTopic.put(name + ApplicationConfig.EVENTS_RECORD_NAME_SUFFIX, ApplicationConfig.EVENTS_RECORD_TYPE);
+            TopicManager.createTopics(eventTopic, applicationConfig);
 
-            final Event eventWriter = new Event(baseName, applicationConfig);
-            cacheEvents.put(baseName, eventWriter);
+            final Event eventWriter = new Event(name, applicationConfig);
+            cacheEvents.put(name, eventWriter);
             return eventWriter;
         }
-    }
-
-    public synchronized void createEvents(final List<String> produceEvents) {
-        final Map<String, String> producerEvents =
-                Stream.of(produceEvents).flatMap(Collection::stream)
-                        .collect(Collectors.toMap(k -> k + ApplicationConfig.EVENTS_RECORD_NAME_SUFFIX, type -> ApplicationConfig.COMMON_RECORD_TYPE,
-                                (command1, command2) -> command1));
-
-        TopicManager.createTopics(producerEvents, applicationConfig);
     }
 }
