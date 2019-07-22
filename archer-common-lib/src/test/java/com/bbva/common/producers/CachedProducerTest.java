94b95b657f492bfae4375e0f6c355519996c81b2
sendoa.moronta <sendoa.moronta.next@bbva.com>
2019-07-22 10:56:53 +0200

    ARCHER-240 add common lib complete tests

--- archer-common-lib/src/test/java/com/bbva/common/producers/CachedProducerTest.java
+++ archer-common-lib/src/test/java/com/bbva/common/producers/CachedProducerTest.java
@@ -1,29 +1,120 @@
-package com.bbva.common.producers.config;
+package com.bbva.common.producers;
 
 import com.bbva.common.config.AppConfiguration;
 import com.bbva.common.config.ApplicationConfig;
+import com.bbva.common.producers.records.GenericRecordImpl;
+import com.bbva.common.producers.records.SpecificRecordImpl;
 import com.bbva.common.util.PowermockExtension;
+import com.bbva.common.utils.headers.RecordHeaders;
+import org.apache.kafka.common.utils.Bytes;
 import org.junit.gen5.api.Assertions;
 import org.junit.gen5.api.DisplayName;
 import org.junit.gen5.api.Test;
 import org.junit.gen5.api.extension.ExtendWith;
 import org.junit.gen5.junit4.runner.JUnit5;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.Future;
 
 @RunWith(JUnit5.class)
 @ExtendWith(PowermockExtension.class)
+@PrepareForTest({DefaultProducer.class, CachedProducer.class})
+@PowerMockIgnore("javax.management.*")
 public class CachedProducerTest {
 
-    @DisplayName("Initialize common configuration")
+    @DisplayName("Create cached producer")
     @Test
-    public void initCommonConfigurationOk() {
-        final ApplicationConfig configuration = new AppConfiguration().init();
+    public void createCahedProducerOk() throws Exception {
 
-        Assertions.assertAll("configurations",
-                () -> Assertions.assertEquals("", configuration.get().get("schema.registry.url")),
-                () -> Assertions.assertEquals("PLAINTEXT://", configuration.get().get("bootstrap.servers")),
-                () -> Assertions.assertEquals("1", configuration.get().get("replication.factor"))
+        final DefaultProducer defaultProducer = Mockito.mock(DefaultProducer.class);
+        PowerMockito.whenNew(DefaultProducer.class).withAnyArguments().thenReturn(defaultProducer);
+
+        final ApplicationConfig configuration = new AppConfiguration().init();
+        final CachedProducer producer = new CachedProducer(configuration);
+
+        Assertions.assertAll("producer",
+                () -> Assertions.assertNotNull(producer)
         );
     }
 
+    @DisplayName("Add message to producer")
+    @Test
+    public void addRecordOk() throws Exception {
+
+        final DefaultProducer defaultProducer = Mockito.mock(DefaultProducer.class);
+        PowerMockito.whenNew(DefaultProducer.class).withAnyArguments().thenReturn(defaultProducer);
+
+        final ApplicationConfig configuration = new AppConfiguration().init();
+        final CachedProducer producer = new CachedProducer(configuration);
+
+        final Future mockedFuture = producer.add(new PRecord<>("test", "key", "value", new RecordHeaders()), null);
+        Assertions.assertAll("producer",
+                () -> Assertions.assertNotNull(producer),
+                () -> Assertions.assertNull(mockedFuture)
+        );
+    }
+
+    @DisplayName("Add message to producer")
+    @Test
+    public void addRecordCompletionOk() throws Exception {
+
+        final DefaultProducer defaultProducer = Mockito.mock(DefaultProducer.class);
+        PowerMockito.whenNew(DefaultProducer.class).withAnyArguments().thenReturn(defaultProducer);
+
+        final ApplicationConfig configuration = new AppConfiguration().init();
+        final CachedProducer producer = new CachedProducer(configuration);
+
+        final Future mockedFuture = producer.add(new PRecord<>("test", "key", "value", new RecordHeaders()), null);
+        Assertions.assertAll("producer",
+                () -> Assertions.assertNotNull(producer),
+                () -> Assertions.assertNull(mockedFuture)
+        );
+    }
+
+
+    @DisplayName("Add and remove message to producer")
+    @Test
+    public void addAndRemoveRecordOk() throws Exception {
+
+        final DefaultProducer defaultProducer = Mockito.mock(DefaultProducer.class);
+        PowerMockito.whenNew(DefaultProducer.class).withAnyArguments().thenReturn(defaultProducer);
+
+        final ApplicationConfig configuration = new AppConfiguration().init();
+        final CachedProducer producer = new CachedProducer(configuration);
+
+        producer.add(new PRecord<>("test", "key", "value", new RecordHeaders()), null);
+        final Future mockedFuture = producer.remove(new PRecord<>("test", "key", "value", new RecordHeaders()), String.class, null);
+
+        Assertions.assertAll("producer",
+                () -> Assertions.assertNotNull(producer),
+                () -> Assertions.assertNull(mockedFuture)
+        );
+    }
+
+
+    @DisplayName("Add message to producer with different serializers")
+    @Test
+    public void addRecordSerializersOk() throws Exception {
+
+        final DefaultProducer defaultProducer = Mockito.mock(DefaultProducer.class);
+        PowerMockito.whenNew(DefaultProducer.class).withAnyArguments().thenReturn(defaultProducer);
+
+        final ApplicationConfig configuration = new AppConfiguration().init();
+        final CachedProducer producer = new CachedProducer(configuration);
+
+        producer.add(new PRecord<>("test", new Integer(1), new Long(1), new RecordHeaders()), null);
+        producer.add(new PRecord<>("test", new Bytes("bytes".getBytes()), ByteBuffer.allocate(1), new RecordHeaders()), null);
+        producer.add(new PRecord<>("test", "bytes".getBytes(), new Boolean(true), new RecordHeaders()), null);
+        final Future mockedFuture = producer.add(new PRecord<>("test", new GenericRecordImpl(), new SpecificRecordImpl(), new RecordHeaders()), null);
+
+        Assertions.assertAll("producer",
+                () -> Assertions.assertNotNull(producer),
+                () -> Assertions.assertNull(mockedFuture)
+        );
+    }
 }
---
+++ archer-common-lib/src/test/java/com/bbva/common/producers/records/SpecificRecordImpl.java
@@ -0,0 +1,22 @@
+package com.bbva.common.producers.records;
+
+import org.apache.avro.Schema;
+import org.apache.avro.specific.SpecificRecord;
+
+public class SpecificRecordImpl implements SpecificRecord {
+
+    @Override
+    public void put(final int i, final Object o) {
+
+    }
+
+    @Override
+    public Object get(final int i) {
+        return null;
+    }
+
+    @Override
+    public Schema getSchema() {
+        return null;
+    }
+}
